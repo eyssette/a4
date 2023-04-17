ba823740addb92959e818b70fd4266306f720ef5
eyssette <cedric.eyssette@gmail.com>
2023-04-17 19:58:45 +0200

    Simplification script pandoc

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var nPages = 1;
 let fontSize = 200;
 function adjustFontSize() {
 	let contentHeight = document.body.offsetHeight;
-	while (contentHeight > 1100 * nPages) {
+	while (contentHeight > 1090 * nPages) {
 		fontSize = fontSize - 0.5;
 		document.body.style.fontSize = fontSize + "px";
 		contentHeight = document.body.offsetHeight;
