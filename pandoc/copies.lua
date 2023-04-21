5e737cb170e063c9a0c3a732dff75a9670d366a7
eyssette <cedric.eyssette@gmail.com>
2023-04-21 14:59:45 +0200

    Vérifie l'existence de meta.copies avant

--- pandoc/copies.lua
+++ pandoc/copies.lua
@@ -1,12 +1,12 @@
 function Meta(meta)
-	n = meta.copies[1]
-	m = tostring(n)
-	for i=1, 30 do
-		if string.find(m,tostring(i)) then
-			j=i
-		end
-	end
 	if meta.copies then
+		n = meta.copies[1]
+		m = tostring(n)
+		for i=1, 30 do
+			if string.find(m,tostring(i)) then
+				j=i
+			end
+		end
 		local a = {}
 		for i=1, j do
 			a[i] = i
