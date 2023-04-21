49d1e7143f5f55feeeecf73df2ddbb4b1ac5c7e7
eyssette <cedric.eyssette@gmail.com>
2023-04-21 15:19:04 +0200

    Return Meta

--- pandoc/copies.lua
+++ pandoc/copies.lua
@@ -1,30 +1,34 @@
 function Meta(meta)
-	if meta then
-		if meta.copies then
-			if type(meta.copies) == "table" then
-				local n = meta.copies[1]
-				local m = tostring(n)
-				for i=1, 30 do
-					if string.find(m,tostring(i)) then
-						j=i
-					end
+	if meta.copies then
+		if type(meta.copies) == "table" then
+			local n = meta.copies[1]
+			local m = tostring(n)
+			for i=1, 30 do
+				if string.find(m,tostring(i)) then
+					j=i
 				end
-				local a = {}
-				for i=1, j do
-					a[i] = i
-				end
-				meta.copies = a
-			elseif type(meta.copies) == "number" then
-				local n = meta.copies
-				local a = {}
-				for i = 1, n do
-				a[i] = i
-				end
-				meta.copies = a
-			else
-				meta.copies = nil
 			end
+			local a = {}
+			for i=1, j do
+				a[i] = i
+			end
+			meta.copies = a
+		elseif type(meta.copies) == "number" then
+			local n = meta.copies
+			local a = {}
+			for i = 1, n do
+			a[i] = i
+			end
+			meta.copies = a
+		else
+			meta.copies = nil
 		end
-		return meta
 	end
+	return meta
 end
+
+return {
+	{
+	  Meta = Meta
+	}
+  }
