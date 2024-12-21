1be8c834f1ea24e9b110569b7a88ce40e86117f0
eyssette <cedric.eyssette@gmail.com>
2024-12-21 17:41:05 +0100

    Amélioration fonction convertLatexExpressions : gestion conflits avec syntaxe Markdown

--- js/processMarkdown/convertLatex.js
+++ js/processMarkdown/convertLatex.js
@@ -1,15 +1,30 @@
 // gestion des mathématiques en Latex
+function convertSpecialExpressionsInLatex(string) {
+	string = string
+		.replace("&#92;[", "")
+		.replace("&#92;]", "")
+		.replace("&#92;(", "")
+		.replace("&#92;)", "")
+		.replaceAll("&lt;", "\\lt")
+		.replaceAll("&gt;", "\\gt")
+		.replaceAll("<em>", "_")
+		.replaceAll("</em>", "_")
+		.replaceAll("&amp;", "&")
+		.replaceAll(" ", "\\ ")
+		.replace(/,/g, "{,}")
+		.replace(/€/g, "\\textrm{€}")
+		.replace(/\\?%/g, "\\%");
+	return string;
+}
+
 export function convertLatexExpressions(string) {
 	return string
 		.replace(/\$\$(.*?)\$\$/g, function (match, equation) {
-			equation = equation.replace(/\\?%/g, "\\%");
-			equation = equation.replace(/€/g, "\\textrm{€}");
-			equation = equation.replace(/,/g, "{,}");
+			equation = convertSpecialExpressionsInLatex(equation);
 			return "&#92;[" + equation + "&#92;]";
 		})
 		.replace(/\$(.*?)\$/g, function (match, equation) {
-			equation = equation.replace(/\\?%/g, "\\%");
-			equation = equation.replace(/€/g, "\\textrm{€}");
+			equation = convertSpecialExpressionsInLatex(equation);
 			return "&#92;(" + equation + "&#92;)";
 		});
 }
