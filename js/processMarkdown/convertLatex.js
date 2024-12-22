5c72b2861d9917cc1fd0a2479c2b316ea21f57eb
Eyssette <cedric.eyssette@gmail.com>
2024-12-22 12:27:27 +0100

    Amélioration convertLatexExpressions : prise en compte multiligne

--- js/processMarkdown/convertLatex.js
+++ js/processMarkdown/convertLatex.js
@@ -1,17 +1,14 @@
 // gestion des mathématiques en Latex
 function convertSpecialExpressionsInLatex(string) {
 	string = string
-		.replace("&#92;[", "")
-		.replace("&#92;]", "")
-		.replace("&#92;(", "")
-		.replace("&#92;)", "")
 		.replaceAll("&lt;", "\\lt")
 		.replaceAll("&gt;", "\\gt")
 		.replaceAll("<em>", "_")
 		.replaceAll("</em>", "_")
 		.replaceAll("&amp;", "&")
-		.replaceAll(" ", "\\ ")
-		.replace(/,/g, "{,}")
+		.replaceAll("<br />", "\\")
+		//.replace(/,/g, "{,}")
+		//.replaceAll(" ", "\\ ")
 		.replace(/€/g, "\\textrm{€}")
 		.replace(/\\?%/g, "\\%");
 	return string;
@@ -19,11 +16,11 @@ function convertSpecialExpressionsInLatex(string) {
 
 export function convertLatexExpressions(string) {
 	return string
-		.replace(/\$\$(.*?)\$\$/g, function (match, equation) {
+		.replace(/\$\$([\s\S]*?)\$\$/gm, function (match, equation) {
 			equation = convertSpecialExpressionsInLatex(equation);
 			return "&#92;[" + equation + "&#92;]";
 		})
-		.replace(/\$(.*?)\$/g, function (match, equation) {
+		.replace(/\$([\s\S]*?)\$/gm, function (match, equation) {
 			equation = convertSpecialExpressionsInLatex(equation);
 			return "&#92;(" + equation + "&#92;)";
 		});
