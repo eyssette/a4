9d01170038e813b827a4b4fed220a497f0a15aad
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-10-06 03:05:02 +0200

    fix: création d'une page avec un blob HTML plutôt que write

--- js/convertToA4/getTemplateAndCreateDocument.js
+++ js/convertToA4/getTemplateAndCreateDocument.js
@@ -4,7 +4,12 @@ export function getTemplateAndCreateDocument(srcTemplate, configTemplate) {
 	//const tempSrc = "https://hebdo.framapad.org/p/5jt64q324p-a9nq/export/txt";
 	const compileTemplate = Handlebars.compile(srcTemplate);
 	const a4html = compileTemplate(configTemplate);
-	const result = window.open("", "_blank");
-	result.document.write(a4html);
-	result.document.close();
+
+	// Créer un blob HTML et ouvre une nouvelle fenêtre avec ce contenu
+	const blob = new Blob([a4html], { type: "text/html" });
+	const url = URL.createObjectURL(blob);
+	window.open(url, "_blank");
+
+	// Libére la mémoire après usage
+	setTimeout(() => URL.revokeObjectURL(url), 10000);
 }
